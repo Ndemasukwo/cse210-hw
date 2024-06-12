4d783ff77b6d561c6983839f67cb2fd6e50d27a7
ndemasukwo <nde23002@byui.edu>
2024-06-12 14:57:57 +0100

    Added a comment and uncommented the first writeline

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,10 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("Hello Prep1 World!");
 
         Console.Write("What is your first name? ");
         string first = Console.ReadLine();
+        // Use the variable after the "write" and before next variable
 
         Console.Write("What is your last name? ");        
         string last = Console.ReadLine();
