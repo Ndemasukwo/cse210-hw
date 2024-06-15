aa0b7e51e3059750b155b2b4c351a0453ae60dbf
ndemasukwo <nde23002@byui.edu>
2024-06-15 17:22:15 +0100

    Week 01 Prep 03

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+        Console.WriteLine("Hello Prep3 World!");        
+
+        // Console.WriteLine($"What is the magic number? {magicNum}");
+        // Console.Write("What is your guess? ");
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+        // string response = Console.ReadLine();
+        // int guess = int.Parse(response);
+        
+        int magicNum = -1;
+
+        while (magicNum != number)
+        {
+            // Console.WriteLine($"What is the magic number? {magicNum}");
+            Console.Write("What is your guess? ");
+            magicNum = int.Parse(Console.ReadLine());
+        
+            if (number > magicNum)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (number < magicNum)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+            // Console.WriteLine($"What is the magic number? {magicNum}");
+            // Console.Write($"What is your guess? ");
+            // response = Console.ReadLine();
+            // guess = int.Parse(response);
+        }       
+    }   
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,14 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        // Console.WriteLine("Hello Prep4 World!");
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished");
+
+        list<int> numbers = new List<int>();
     }
 }
