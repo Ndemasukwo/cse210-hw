8da84cb1cb06c8bc88af94ce83182de2ee37fc10
ndemasukwo <nde23002@byui.edu>
2024-06-17 18:43:34 +0100

    Week 01 Prep 04

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -6,9 +6,39 @@ class Program
     static void Main(string[] args)
     {
         // Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
 
-        Console.WriteLine("Enter a list of numbers, type 0 when finished");
+        int givenNum = -1;
+        while (givenNum != 0)
+        {
+            Console.WriteLine("Enter a list of numbers, type 0 when finished: ");
+            string response = Console.ReadLine();
+            int userNum = int.Parse(response);
 
-        list<int> numbers = new List<int>();
+            if (givenNum != 0)
+            {
+                numbers.Add(givenNum);
+            }
+        }
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+
+        Console.WriteLine($"The sum is: {sum}");
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers[0];
+
+        foreach (int num in numbers)
+        {
+            if (num > max)
+            {
+                max = num;
+            }
+        }
+        Console.WriteLine($"The max is: {max}");
     }
 }
