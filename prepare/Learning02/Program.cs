1d180a5e0cea1aad8caa17e57bc44896e296b791
ndemasukwo <nde23002@byui.edu>
2024-07-09 17:51:46 +0100

    Modified folders and Week 2 Learning Program cs

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,48 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Journal myJournal = new Journal();
-        PromptGenerator promptGenerator = new PromptGenerator();
-        string userChoice;
+        // Console.WriteLine("Hello Learning02 World!");
+        // Instance
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
 
-        do
-        {
-            Console.WriteLine("Menu:");
-            Console.WriteLine("1. Write a new entry");
-            Console.WriteLine("2. Display the journal");
-            Console.WriteLine("3. Save the journal to a file");
-            Console.WriteLine("4. Load the journal from a file");
-            Console.WriteLine("5. Exit");
-            Console.Write("Enter your choice: ");
-            userChoice = Console.ReadLine();
+        job1.DisplayJobDetails();
 
-            switch (userChoice)
-            {
-                case "1":
-                    string prompt = promptGenerator.GetRandomPrompt();
-                    Console.WriteLine(prompt);
-                    string response = Console.ReadLine();
-                    string date = DateTime.Now.ToShortDateString();
-                    Entry entry = new Entry(date, prompt, response);
-                    myJournal.AddEntry(entry);
-                    break;
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
 
-                case "2":
-                    myJournal.DisplayAll();
-                    break;
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
 
-                case "3":
-                    Console.Write("Enter filename to save the journal: ");
-                    string saveFilename = Console.ReadLine();
-                    myJournal.SaveToFile(saveFilename);
-                    break;
+        myResume.Dispaly();
 
-                case "4":
-                    Console.Write("Enter filename to load the journal: ");
-                    string loadFilename = Console.ReadLine();
-                    myJournal.LoadFromFile(loadFilename);
-                    break;
-            }
-        } while (userChoice != "5");
     }
 }
