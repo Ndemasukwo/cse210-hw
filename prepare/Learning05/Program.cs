3501ba9942b1c14756d82f810045236d98f15738
ndemasukwo <nde23002@byui.edu>
2024-07-17 15:14:08 +0100

    Week 5 Learning

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -1,9 +1,21 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Square square = new Square("Red", 4.0);
+        Console.WriteLine($"Square Color: {square.Color}, Area: {square.GetArea()}");
+
+        Square square = new Square("Red", 4.0);
+        Console.WriteLine($"Square Color: {square.Color}, Area: {square.GetArea()}");
+
+        Rectangle rectangle = new Rectangle("Blue", 3.0, 5.0);
+        Console.WriteLine($"Rectangle Color: {rectangle.Color}, Area: {rectangle.GetArea()}");
+
+        Circle circle = new Circle("Green", 2.5);
+        Console.WriteLine($"Circle Color: {circle.Color}, Area: {circle.GetArea()}");
+
     }
 }
